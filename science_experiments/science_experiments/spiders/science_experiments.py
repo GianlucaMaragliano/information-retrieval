f736ccb6c66a85b677a720601b3b8c6094453e31
Gianluca <gianluca.maragliano@usi.ch>
2023-10-23 17:02:26 +0200

    started implementing crawler

--- science_experiments/science_experiments/spiders/science_experiments.py
+++ science_experiments/science_experiments/spiders/science_experiments.py
@@ -1,7 +1,24 @@
+from typing import Any
 import scrapy
+from scrapy.http import Response
+
+
+# eval "$(/home/gianluca/anaconda3/bin/conda shell.bash hook)"
+# conda activate scrapy_env
+# scrapy crawl science_experiments -o science_experiments.json
 
 class ScienceExperimentsSpider(scrapy.Spider):
 
     name = 'science_experiments'
 
-    start_urls = [""]
+    start_urls = ["https://www.sciencebuddies.org/science-experiments?p=1"]
+
+    def parse(self, response: Response):
+
+
+        # for experiment in response.xpath("//div[@class='search-result search-result-grid page-break-avoid']"):
+        experiment = response.xpath("//div[@class='search-result search-result-grid page-break-avoid']")[0]
+        yield {
+            "titles": experiment.xpath("//span[@class='title-name']/text()").getall(),
+            # "link": experiment.xpath(".//a/@href"),
+            }
