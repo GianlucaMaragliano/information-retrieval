87c779d1976561f25aa77b0d3909757107291a5b
Gianluca <gianluca.maragliano@usi.ch>
2023-10-24 11:01:48 +0200

    created spider for stevespangler.com/experiments/

---
+++ science_experiments/science_experiments/spiders/steve_spangler.py
@@ -0,0 +1,27 @@
+from typing import Any
+import scrapy
+from scrapy.http import Response
+
+class steveSpanglerSpider(scrapy.Spider):
+    name = "steve_spangler"
+
+    start_urls = ["https://stevespangler.com/experiments/"]
+
+    def parse(self, response: Response):
+
+        for experiment in response.xpath("//article[@class='post']"):
+
+            title = experiment.xpath(".//div[@class='text']/h3/text()").get()
+            description = experiment.xpath(".//div[@class='text']/p/text()").get()
+            link = experiment.xpath(".//div[@class='text']/div/a[@class='wp-block-button__link']/@href/text()").get()
+
+            yield {
+                "title": title,
+                "description": description,
+                # "link": link,
+            }
+
+            next_page = response.xpath("//a[@class='nextpostslink']/@href").get()
+
+            if next_page:
+                yield response.follow(url=next_page, callback=self.parse)
