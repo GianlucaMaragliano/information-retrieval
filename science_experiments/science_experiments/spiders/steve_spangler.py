7a787d54a2e381ebc0abd948a4022f4673a35820
Gianluca <gianluca.maragliano@usi.ch>
2023-10-25 09:06:44 +0200

    added link to steve_spangler stuff retrieved

--- science_experiments/science_experiments/spiders/steve_spangler.py
+++ science_experiments/science_experiments/spiders/steve_spangler.py
@@ -9,16 +9,18 @@ class steveSpanglerSpider(scrapy.Spider):
 
     def parse(self, response: Response):
 
+        base_url = "https://stevespangler.com"
+
         for experiment in response.xpath("//article[@class='post']"):
 
             title = experiment.xpath(".//div[@class='text']/h3/text()").get()
             description = experiment.xpath(".//div[@class='text']/p/text()").get()
-            link = experiment.xpath(".//div[@class='text']/div/a[@class='wp-block-button__link']/@href/text()").get()
+            link = experiment.xpath(".//div[@class='text']/div/a[@class='wp-block-button__link']/@href").get()
 
             yield {
                 "title": title,
                 "description": description,
-                # "link": link,
+                "link": base_url+link,
             }
 
             next_page = response.xpath("//a[@class='nextpostslink']/@href").get()
