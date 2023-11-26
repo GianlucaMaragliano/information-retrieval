4109d1889baa255b9733e87d6ff3b27cf7c66c00
Gianluca <gianluca.maragliano@usi.ch>
2023-11-26 18:47:31 +0100

    pacchetti vari

---
+++ frontend/src/components/ResultList.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+  data() {
+
+  },
+  computed: {
+    results() {
+      return this.$store.state.results
+    }
+  },
+  watch: {
+    results: {
+      immediate: false,
+      deep: false,
+      handler(newValue, oldValue) {
+        console.log(newValue);
+      }
+    }
+  },
+}
+</script>
+
+<template>
+  <ul v-for="item in results">
+    <a v-bind:href=item.Link>
+      {{item.Title}}
+    </a>
+  </ul>
+
+</template>
+
+<style scoped>
+
+</style>
