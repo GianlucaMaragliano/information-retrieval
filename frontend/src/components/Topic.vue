c3b39ac1be118eb72a957285f3dbbbe03f8cba29
Raffaele <raffaele.perri@usi.ch>
2023-11-28 11:06:49 +0100

    changes

--- frontend/src/components/Topic.vue
+++ frontend/src/components/Topic.vue
@@ -4,9 +4,21 @@ import Document from "@/components/Document.vue";
 
 export default {
   components: {Document},
+  data () {
+    return {
+      clicked: false,
+      isArrowRotated: false,
+    }
+  },
   props: {
     results: Array,
     topic: String
+  },
+  methods: {
+    show() {
+      this.clicked = !this.clicked;
+      this.isArrowRotated = !this.isArrowRotated;
+    }
   },
   computed: {
     ineherent_res() {
@@ -34,12 +46,39 @@ export default {
 <template>
 
   <h2> {{this.topic}}: {{ineherent_res.length}} result </h2>
-  <ul v-for="exp in ineherent_res">
-    <Document :document="exp"></Document>
+  <div @click="show">
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        class="arrow-icon"
+    >
+      <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M5 10l7-7m0 0l7 7m-7-7v18"
+      />
+    </svg>
+  </div>
+  <div v-if="clicked">
+
+  </div>
+
+  <ul v-else v-for="exp in ineherent_res">
+      <Document :document="exp"></Document>
   </ul>
 
 </template>
 
 <style scoped>
-
+  .arrow-icon {
+    width: 24px;
+    height: 24px;
+    fill: none;
+    stroke: hsl(208, 100%, 43%);
+    transition: transform 0.3s;
+    transform: rotate(180deg); /* Rotate the arrow when the class is present */
+  }
 </style>
